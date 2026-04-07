ed666f9847adf6465b1d5a4255f103e8a0a93ca3
Papa Abdoulaye <papaabdoulaye.ndoye.01@gmail.com>
2026-04-07 23:42:29 +0200

    Implementing TradeServiceTest with mock data

--- src/test/java/victus/rtrackers/TradeServiceTest.java
+++ src/test/java/victus/rtrackers/TradeServiceTest.java
@@ -6,12 +6,15 @@ import victus.rtrackers.model.Trade;
 import victus.rtrackers.repository.TradeRepository;
 import victus.rtrackers.services.TradeService;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TradeServiceTest {
     private TradeRepository mockRepository = Mockito.mock(TradeRepository.class);
     private RiskCalculator mockRiskCalculator = Mockito.mock(RiskCalculator.class);
-    private TradeService service = new TradeService(mockRepository,mockRiskCalculator);
+
+    private TradeService service = new TradeService(mockRiskCalculator,mockRepository);
 
     @Test
     void should_return_high_risk_when_exposure_over_100k() {
@@ -30,7 +33,13 @@ public class TradeServiceTest {
 
     @Test
     void should_calculate_total_exposure() {
-
+        List<Trade> trades = List.of(
+                new Trade("AAPL", 100, 50),
+                new Trade("USD", 200, 100)
+        );
+        Mockito.when(mockRepository.findAll()).thenReturn(trades);
+        double exposure = service.getTotalExposure();
+        assertEquals(25_000, exposure);
     }
 
 }
